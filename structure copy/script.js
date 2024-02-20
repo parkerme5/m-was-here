89458659f5ad18a70b11592cedf584621ac950b9
parkerme5 <157614140+parkerme5@users.noreply.github.com>
2024-02-20 13:15:39 -0500

    structure project

---
+++ structure copy/script.js
@@ -0,0 +1,12 @@
+const videos = document.querySelectorAll('.video');
+
+videos.forEach(video => {
+  video.addEventListener('mouseenter', () => {
+    video.play();
+  });
+  
+  video.addEventListener('mouseleave', () => {
+    video.pause();
+    video.currentTime = 0;
+  });
+});
