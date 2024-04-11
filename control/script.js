0a2310fc10c137133526335af642a30aa908302c
parkerme5 <157614140+parkerme5@users.noreply.github.com>
2024-04-11 18:01:25 -0400

    update

---
+++ control/script.js
@@ -0,0 +1,30 @@
+$(".candle-1").click(function(){
+$(".flame-1").toggleClass("flame-on");
+});
+
+$(".candle-2").click(function(){
+    $(".flame-2").toggleClass("flame-on");
+    });
+
+    $(".candle-3").click(function(){
+        $(".flame-3").toggleClass("flame-on");
+        });
+        $(".candle-4").click(function(){
+            $(".flame-4").toggleClass("flame-on");
+            });
+
+            $(".candle-5").click(function(){
+                $(".flame-5").toggleClass("flame-on");
+                });
+
+                $(".window").click(function(){
+                    $(".window-open").toggleClass("window-open-on");
+                    });
+
+                    $(".switch").click(function(){
+                        $(".dark-portrait").toggleClass("dark-portrait-on");
+                        });
+
+                   
+                
+
